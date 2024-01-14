c570d96fbd3401b8a31690108e38139bd9928621
pedram-macbook <pedram@email.com>
2024-01-14 19:53:50 +0330

    add model to home app. add serializers file and response model query to serializers

--- djangorest/home/views.py
+++ djangorest/home/views.py
@@ -1,7 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Person
+from .serializers import PersonSerializer
 
 
 class HomeView(APIView):
     def get(self , request):
-        return Response({'name':'pedram'})
+        persons = Person.objects.all()
+        serz_data = PersonSerializer(instance=persons , many=True)
+        return Response(data=serz_data.data)
