4476c0ba05af8767e4ee1671e43371c0eab44d58
pedram-macbook <pedram@email.com>
2024-01-14 13:22:52 +0330

    add view for accounts and home

--- djangorest/accounts/views.py
+++ djangorest/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+
+class UserRegisterView(View):
+    def get(self , request):
+        pass
+
+    def post(self , request):
+        pass
