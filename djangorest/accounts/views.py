e77f768c9fae4354dffdd4bc265365d70c75f91b
pedram-macbook <pedram@email.com>
2024-01-15 12:26:22 +0330

    add status

--- djangorest/accounts/views.py
+++ djangorest/accounts/views.py
@@ -1,7 +1,7 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
+from rest_framework import status
 from .serializers import UserRegisterSerializer
 
 class UserRegister(APIView):
@@ -10,5 +10,5 @@ class UserRegister(APIView):
         serz_data = UserRegisterSerializer(data=request.POST)
         if serz_data.is_valid():
             serz_data.create(serz_data.validated_data)
-            return Response(serz_data.data)
+            return Response(serz_data.data , status=status.HTTP_201_CREATED)
         return Response(serz_data.errors)
