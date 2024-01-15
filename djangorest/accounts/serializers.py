3840f3145b6fb4fe45c2f4daefdf19ff46912927
pedram-macbook <pedram@email.com>
2024-01-15 10:57:39 +0330

    add field_level validation for username . add object_level validation for password and password2

--- djangorest/accounts/serializers.py
+++ djangorest/accounts/serializers.py
@@ -4,5 +4,16 @@ from rest_framework import serializers
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
-    password = serializers.CharField(required=True , write_only=True)
+    password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
 
+    # field level validations :
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('username cant be admin')
+        return value
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError('password must match')
+        return data
