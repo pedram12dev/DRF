0eaa92c66efbb427cb38f6b5bdda2a12567ea240
pedram-macbook <pedram@email.com>
2024-01-16 20:24:29 +0330

    add question and answer serializers. write question CRUD view and urls

--- djangorest/home/urls.py
+++ djangorest/home/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('' , views.HomeView.as_view() , name='home'),
+    path('question/' , views.QuestionView.as_view()),
+    path('question/<int:pk>/' , views.QuestionView.as_view()),
 ]
--- djangorest/home/views.py
+++ djangorest/home/views.py
@@ -1,11 +1,38 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Question, Answer
+from .serializers import PersonSerializer, QuestionSerializer, AnswerSerializer
 
 
 class HomeView(APIView):
-    def get(self , request):
+    def get(self, request):
         persons = Person.objects.all()
-        serz_data = PersonSerializer(instance=persons , many=True)
+        serz_data = PersonSerializer(instance=persons, many=True)
         return Response(data=serz_data.data)
+
+
+class QuestionView(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        serz_data = QuestionSerializer(instance=questions, many=True).data
+        return Response(data=serz_data)
+
+    def post(self, request):
+        serz_data = QuestionSerializer(data=request.data)
+        if serz_data.is_valid():
+            serz_data.save()
+            return Response(serz_data.data)
+        return Response(serz_data.errors)
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        serz_data = QuestionSerializer(instance=question , data=request.data , partial=True)
+        if serz_data.is_valid():
+            serz_data.save()
+            return Response(serz_data.data)
+        return Response(serz_data.errors)
+
+    def delete(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        question.delete()
+        return Response({'message': 'question is deleted'})
